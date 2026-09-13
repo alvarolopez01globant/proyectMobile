27df9519cd4782748995a425256ddf597b4cbc3b
alvaro.lopez01 <alvaro.lopez01@globant.com>
2026-09-13 18:52:48 -0500

    second test commit

---
+++ src/test/java/org/example/pages/BasePage.java
@@ -0,0 +1,42 @@
+package org.example.pages;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    protected AndroidDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(AndroidDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void waitForVisibility(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void click(WebElement element) {
+        waitForVisibility(element);
+        element.click();
+    }
+
+    public void sendKeys(WebElement element, String text) {
+        waitForVisibility(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getText(WebElement element) {
+        waitForVisibility(element);
+        return element.getText();
+    }
+}
