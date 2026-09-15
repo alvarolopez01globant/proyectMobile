a6e2d9da86193dfeb437a408dc4fd5657d7fa512
alvaro.lopez01 <alvaro.lopez01@globant.com>
2026-09-14 22:53:36 -0500

    changing selector mode

--- src/test/java/org/example/automation/tests/SignUpTest.java
+++ src/test/java/org/example/automation/tests/SignUpTest.java
@@ -1,6 +1,7 @@
 package org.example.automation.tests;
 
 import org.example.automation.pages.LoginPage;
+import org.example.automation.pages.NavigationBarPage;
 import org.example.automation.utils.BaseTest;
 import org.example.automation.utils.DriverManager;
 import org.testng.Assert;
@@ -12,11 +13,14 @@ public class SignUpTest extends BaseTest {
 
     @Test
     public void shouldSignUpSuccessfully() {
+        NavigationBarPage navigationBarPage = new NavigationBarPage(DriverManager.getDriver());
+        navigationBarPage.loginIcon.click();
+
         LoginPage loginPage = new LoginPage(DriverManager.getDriver());
         String email = "user." + UUID.randomUUID() + "@mail.com";
 
         loginPage.signUp(email, "Password123");
 
-        Assert.assertTrue(loginPage.alertTitle.isDisplayed(), "Signup should complete successfully");
+        Assert.assertTrue(loginPage.isSuccessPopupVisible(), "Signup should complete successfully");
     }
 }
